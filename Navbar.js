d0a112fc488dfde4dc2ad11245c39b22386979eb
diegocoronado-cloud <diego.coronado@student.uagc.edu>
2025-06-15 10:12:32 +0300

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import { FaHome, FaFilm, FaShoppingCart, FaInfoCircle } from "react-icons/fa";
+import { FaHome, FaFilm, FaShoppingCart, FaInfoCircle, FaBoxOpen } from "react-icons/fa";
 
-const Navbar = () => {
+const Navbar = ({ cart }) => {
+  const itemCount = cart?.reduce((sum, item) => sum + item.quantity, 0) || 0;
+
   return (
     <nav className="navbar">
       <h1 className="logo">StreamList</h1>
       <ul className="nav-links">
         <li><Link to="/"><FaHome /> Home</Link></li>
         <li><Link to="/movies"><FaFilm /> Movies</Link></li>
-        <li><Link to="/cart"><FaShoppingCart /> Cart</Link></li>
+        <li><Link to="/subscriptions"><FaBoxOpen /> Subscriptions</Link></li>
+        <li><Link to="/cart"><FaShoppingCart /> Cart ({itemCount})</Link></li>
         <li><Link to="/about"><FaInfoCircle /> About</Link></li>
       </ul>
     </nav>
