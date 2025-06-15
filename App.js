6bc1e0fc3b70373ec297afc4c2704cbf421e5163
diegocoronado-cloud <diego.coronado@student.uagc.edu>
2025-06-15 10:14:27 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import StreamList from "./components/StreamList";
 import Movies from "./components/Movies";
 import Cart from "./components/Cart";
 import About from "./components/About";
+import Subscriptions from "./components/Subscriptions";
 import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState(() => {
+    const stored = localStorage.getItem("cart");
+    return stored ? JSON.parse(stored) : [];
+  });
+
   return (
     <Router>
-      <Navbar />
+      <Navbar cart={cart} />
       <div className="main-content">
         <Routes>
           <Route path="/" element={<StreamList />} />
           <Route path="/movies" element={<Movies />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
+          <Route path="/subscriptions" element={<Subscriptions cart={cart} setCart={setCart} />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
