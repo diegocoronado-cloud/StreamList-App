713fde6045a0abaf8d4aac3da0a89e59c5edd1c6
diegocoronado-cloud <diego.coronado@student.uagc.edu>
2025-06-15 02:11:34 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 1,
+    name: "EZTech Monthly Subscription",
+    price: 29.99,
+    type: "subscription",
+    description: "Access all premium tech services monthly",
+  },
+  {
+    id: 2,
+    name: "EZTech Yearly Subscription",
+    price: 299.99,
+    type: "subscription",
+    description: "Access all premium tech services yearly",
+  },
+  {
+    id: 3,
+    name: "EZTech Shirt",
+    price: 19.99,
+    type: "accessory",
+    description: "Comfortable branded techwear shirt",
+  },
+  {
+    id: 4,
+    name: "EZTech Phone Case",
+    price: 14.99,
+    type: "accessory",
+    description: "Protective and stylish phone case",
+  }
+];
